a460cf930f7b230c7018947a59d219765cbdbc6e
Alex Allen <allenalex05@gmail.com>
2014-04-28 16:57:53 -0500

    initial project version

---
+++ app.js
@@ -0,0 +1,23 @@
+var assert = require("assert");
+
+(function(){
+	this.cl = function(string){
+		console.log(string);
+	}
+})();
+
+function num1(){
+	return 3;
+}
+
+function num2(){
+	return 4;
+}
+
+var add = function(a, b){
+	return a() + b();
+}
+
+var x = add(num1, num2);
+
+cl(x);
